68e00af2839e6216c13f93d3c47d28bec7cc3314
garsonlab <jiacheng_liu@yeah.net>
2018-06-04 20:35:22 +0800

    First Commit

---
+++ GSerach/GSearchForm.Designer.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GSerach
+{
+    partial class GSearchForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GSearchForm));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.other = new System.Windows.Forms.TextBox();
+            this.cs = new System.Windows.Forms.CheckBox();
+            this.txt = new System.Windows.Forms.CheckBox();
+            this.lua = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.searchContent = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.path = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tool1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tool2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.other);
+            this.groupBox1.Controls.Add(this.cs);
+            this.groupBox1.Controls.Add(this.txt);
+            this.groupBox1.Controls.Add(this.lua);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.searchContent);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnOpenFolder);
+            this.groupBox1.Controls.Add(this.path);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(492, 116);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search Info";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(250, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Other:";
+            // 
+            // other
+            // 
+            this.other.Location = new System.Drawing.Point(289, 75);
+            this.other.Name = "other";
+            this.other.Size = new System.Drawing.Size(147, 21);
+            this.other.TabIndex = 10;
+            this.other.MouseEnter += new System.EventHandler(this.other_MouseEnter);
+            // 
+            // cs
+            // 
+            this.cs.AutoSize = true;
+            this.cs.Location = new System.Drawing.Point(196, 77);
+            this.cs.Name = "cs";
+            this.cs.Size = new System.Drawing.Size(48, 16);
+            this.cs.TabIndex = 9;
+            this.cs.Text = "*.cs";
+            this.cs.UseVisualStyleBackColor = true;
+            // 
+            // txt
+            // 
+            this.txt.AutoSize = true;
+            this.txt.Location = new System.Drawing.Point(136, 77);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(54, 16);
+            this.txt.TabIndex = 8;
+            this.txt.Text = "*.txt";
+            this.txt.UseVisualStyleBackColor = true;
+            // 
+            // lua
+            // 
+            this.lua.AutoSize = true;
+            this.lua.Location = new System.Drawing.Point(76, 77);
+            this.lua.Name = "lua";
+            this.lua.Size = new System.Drawing.Size(54, 16);
+            this.lua.TabIndex = 7;
+            this.lua.Text = "*.lua";
+            this.lua.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Extends:";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(364, 48);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // searchContent
+            // 
+            this.searchContent.Location = new System.Drawing.Point(107, 48);
+            this.searchContent.Name = "searchContent";
+            this.searchContent.Size = new System.Drawing.Size(254, 21);
+            this.searchContent.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Search Parms:";
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(408, 18);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(31, 23);
+            this.btnOpenFolder.TabIndex = 2;
+            this.btnOpenFolder.Text = "...";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(76, 20);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(326, 21);
+            this.path.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(15, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Directory:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 135);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(491, 183);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            this.dataGridView1.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPosPaint);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tool1,
+            this.tool2});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(173, 48);
+            // 
+            // tool1
+            // 
+            this.tool1.Name = "tool1";
+            this.tool1.Size = new System.Drawing.Size(172, 22);
+            this.tool1.Text = "打开文件";
+            this.tool1.Click += new System.EventHandler(this.tool1_Click);
+            // 
+            // tool2
+            // 
+            this.tool2.Name = "tool2";
+            this.tool2.Size = new System.Drawing.Size(172, 22);
+            this.tool2.Text = "打开文件所在位置";
+            this.tool2.Click += new System.EventHandler(this.tool2_Click);
+            // 
+            // GSearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 320);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GSearchForm";
+            this.Text = "GSearch (文本内容搜索)";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private Label label2;
+        private Button btnSearch;
+        private TextBox searchContent;
+        private Label label3;
+        private Label label4;
+        private TextBox other;
+        private CheckBox cs;
+        private CheckBox txt;
+        private CheckBox lua;
+        private DataGridView dataGridView1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem tool1;
+        private ToolStripMenuItem tool2;
+        private ToolTip toolTip1;
+    }
+}
+
---
+++ GSerach/GSearchForm.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GSerach
+{
+    public partial class GSearchForm : Form
+    {
+        public GSearchForm()
+        {
+            InitializeComponent();
+        }
+
+        private string lastFolder = "";
+        private DataTable dt = new DataTable();
+        private string selectPath;
+        private string fullPath;
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "Select Folder To Search";
+            if (string.IsNullOrEmpty(lastFolder))
+                dialog.RootFolder = Environment.SpecialFolder.Desktop;
+            else
+                dialog.SelectedPath = lastFolder;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                path.Text = dialog.SelectedPath;
+                lastFolder = dialog.SelectedPath;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(path.Text))
+            {
+                MessageBox.Show("Not Exist Directory!");
+                return;
+            }
+            lastFolder = path.Text;
+
+            if (string.IsNullOrEmpty(searchContent.Text.Trim()))
+            {
+                MessageBox.Show("Search Content is NULL!");
+                return;
+            }
+
+            Dictionary<string, int> searchEtds = new Dictionary<string, int>();
+            if(lua.Checked)
+                searchEtds.Add(".lua", 1);
+            if(cs.Checked)
+                searchEtds.Add(".cs", 1);
+            if(txt.Checked)
+                searchEtds.Add(".txt", 1);
+
+            if (!string.IsNullOrEmpty(other.Text))
+            {
+                string[] sps = other.Text.Split('|');
+                foreach (var sp in sps)
+                {
+                    if(!searchEtds.ContainsKey(sp.ToLower()))
+                        searchEtds.Add(sp.ToLower(), 1);
+                }
+            }
+            
+            dt.Reset();
+            dt.Columns.Add(new DataColumn("文件名", typeof(string)));
+            dt.Columns.Add(new DataColumn("行列", typeof(string)));
+            dt.Columns.Add(new DataColumn("匹配内容", typeof(string)));
+            dt.Columns.Add(new DataColumn("创建时间", typeof(DateTime)));
+            dt.Columns.Add(new DataColumn("最后修改时间", typeof(DateTime)));
+            dt.Columns.Add(new DataColumn("路径", typeof(string)));
+
+
+            string[] paths = Directory.GetFiles(lastFolder, "*.*", SearchOption.AllDirectories);
+            foreach (var p in paths)
+            {
+                string etd = Path.GetExtension(p).ToLower();
+                if (searchEtds.Count == 0 || searchEtds.ContainsKey(etd))
+                {
+                    string[] fileTxt = File.ReadAllLines(p);
+                    FileInfo file = new FileInfo(p);
+                    string name = Path.GetFileName(p);
+
+                    for (int i = 0; i < fileTxt.Length; i++)
+                    {
+                        MatchCollection matchs = Regex.Matches(fileTxt[i], searchContent.Text);
+                        foreach (Match match in matchs)
+                        {
+                            DataRow row = dt.NewRow();
+                            row[0] = name;
+                            row[1] = string.Format("{0}:{1}", i + 1, match.Index + 1);
+                            row[2] = match.Value;
+                            row[3] = file.CreationTime;
+                            row[4] = file.LastWriteTime;
+                            row[5] = p.Replace("\\", "/");
+                            dt.Rows.Add(row);
+                        }
+                    }
+                }
+            }
+
+            dataGridView1.DataSource = dt;
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right && e.ColumnIndex > -1 && e.RowIndex > -1)  //点击的是鼠标右键，并且不是表头
+            {
+                selectPath = this.dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+                var name = this.dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+                fullPath = selectPath;
+                selectPath = selectPath.Replace(name, ""); 
+                //右键选中单元格
+                this.dataGridView1.Rows[e.RowIndex].Selected = true;
+                this.contextMenuStrip1.Show(MousePosition.X, MousePosition.Y); //MousePosition.X, MousePosition.Y 是为了让菜单在所选行的位置显示
+            }
+        }
+
+        private void tool1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(fullPath);
+        }
+
+        private void tool2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(selectPath);
+        }
+
+        private void dataGridView1_RowPosPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            DataGridView dgv = sender as DataGridView;
+            Rectangle rectangle = new Rectangle(e.RowBounds.Location.X,
+                                                e.RowBounds.Location.Y,
+                                                dgv.RowHeadersWidth - 4,
+                                                e.RowBounds.Height);
+
+
+            TextRenderer.DrawText(e.Graphics, (e.RowIndex + 1).ToString(),
+                                    dgv.RowHeadersDefaultCellStyle.Font,
+                                    rectangle,
+                                    dgv.RowHeadersDefaultCellStyle.ForeColor,
+                                    TextFormatFlags.VerticalCenter | TextFormatFlags.Right);
+        }
+
+        private void other_MouseEnter(object sender, EventArgs e)
+        {
+            toolTip1.Show("自定义后缀，多个‘|’分隔，如 .txt|.csv", other);
+        }
+    }
+}
